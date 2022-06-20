4dfce3faf7cf5c398e1f3ff2a5c7e8ab8a8e63fd
ekaterinamh <98652253+ekaterinamh@users.noreply.github.com>
2022-06-20 22:02:01 +0300

    Add files via upload

---
+++ Практическая 1/Zadanie2/src/Zadanie2.java
@@ -0,0 +1,8 @@
+
+public class Zadanie2 {
+	 public static void main(String args[]) { 
+	      for(int i = 0; i < args.length; i++) {
+	         System.out.println("Вы ввели" + i + "параметров");
+	      }
+	   }
+}
