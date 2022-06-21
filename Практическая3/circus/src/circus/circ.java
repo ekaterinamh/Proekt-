af1e1d3b00de2c3e27841a48eab9757e8718b63c
ekaterinamh <98652253+ekaterinamh@users.noreply.github.com>
2022-06-22 01:50:03 +0300

    Add files via upload

---
+++ Практическая3/circus/src/circus/circ.java
@@ -0,0 +1,8 @@
+package circus;
+
+public class circ {
+	public static void main (String[] args) {
+		circ cir = new circ ("Огни", "Самара", "03.04.2022", 1500,"Павлов Алексей","Фэнтэзи",50);
+		System.out.println(cir);
+		}
+}
